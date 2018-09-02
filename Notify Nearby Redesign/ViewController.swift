4af9dc2412df4a67c6b90e98c50e97929e9d278f
Noman Ikram <nomanikram0@icloud.com>
2018-09-02 19:23:35 +0500

    Initial Commit

---
+++ Notify Nearby Redesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notify Nearby Redesign
+//
+//  Created by Noman Ikram on 01/09/2018.
+//  Copyright © 2018 nomanikram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
